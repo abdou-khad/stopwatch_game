d71f4be30725b17213f72ef7c28f7f1e33b6d283
abdou <khaabdou23@gmail.com>
2022-09-01 14:42:22 +0000

    game updated

--- app.js
+++ app.js
@@ -9,18 +9,30 @@ function increment(){
 
    
 }
-
-
-function start(){
+document.querySelector(".btn1").addEventListener("click",()=>{
     increment();
-}
+})
 
-function stop(){
+document.querySelector(".btn2").addEventListener("click",()=>{
     clearTimeout(timeout);
-}
+})
 
-function reset(){
+document.querySelector(".btn3").addEventListener("click",()=>{
     clearTimeout(timeout);
     count = 0;
     document.querySelector(".counter").innerHTML = count;
-}
+})
+
+// function start(){
+//     increment();
+// }
+
+// function stop(){
+//     clearTimeout(timeout);
+// }
+
+// function reset(){
+//     clearTimeout(timeout);
+//     count = 0;
+//     document.querySelector(".counter").innerHTML = count;
+// }
