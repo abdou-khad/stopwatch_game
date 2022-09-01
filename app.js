23b250b6d1eb88a4793340d2092a98631b7803ac
abdou <khaabdou23@gmail.com>
2022-09-01 16:49:43 +0000

    game updated

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
-let count = 0;
-let timeout;
+let count_left = '00';
+let count_right = '00';
+let timeout_right;
+let timeout_left;
 
 function increment(){
-    count++;
-    let prop = document.getElementsByClassName("counter")[0];
-    prop.innerHTML = count;
-    timeout = setTimeout(increment,1000);
+    count_right++;
+    let prop_right = document.querySelector(".right");
+    prop_right.innerHTML = count_right;
+    timeout_right = setTimeout(increment,1000);
+
+    if(count_right > 99){
+        clearTimeout(timeout_right);
+        count_right = '00';
+        count_left++;
+        let prop_left = document.querySelector(".left");
+        prop_left.innerHTML = count_left;
+        timeout_right = setTimeout(increment,1000);
+
+
+    }
 
    
 }
+//start counter
 document.querySelector(".btn1").addEventListener("click",()=>{
     increment();
 })
-
+//stop counter
 document.querySelector(".btn2").addEventListener("click",()=>{
-    clearTimeout(timeout);
+    clearTimeout(timeout_right);
+    clearTimeout(timeout_left);
 })
-
+//reset counter
 document.querySelector(".btn3").addEventListener("click",()=>{
-    clearTimeout(timeout);
-    count = 0;
-    document.querySelector(".counter").innerHTML = count;
+    clearTimeout(timeout_right);
+    clearTimeout(timeout_left);
+    count_left = '00';
+    count_right = '00';
+    document.querySelector(".right").innerHTML = count_right;
+    document.querySelector(".left").innerHTML = count_left;
 })
 
-// function start(){
-//     increment();
-// }
-
-// function stop(){
-//     clearTimeout(timeout);
-// }
-
-// function reset(){
-//     clearTimeout(timeout);
-//     count = 0;
-//     document.querySelector(".counter").innerHTML = count;
-// }
